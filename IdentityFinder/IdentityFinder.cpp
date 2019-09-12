2ae829330fcb941d6922292026e51a5dd6e84051
RyogoTabuchi <ryogo.1008@gmail.com>
2019-09-12 12:21:40 +0900

    add some example
    IdentityFinderによる結果を確かめた

--- IdentityFinder/IdentityFinder.cpp
+++ IdentityFinder/IdentityFinder.cpp
@@ -18,6 +18,8 @@ template<class T>void print_vector(vector<T> vec){
   cout<<endl;
 }
 
+
+
 bool strict(list<int> lst){
   if(lst.empty())return true;
   for(list<int>::iterator itr=lst.begin();next(itr)!=lst.end();itr++){
@@ -25,9 +27,72 @@ bool strict(list<int> lst){
   }
   return true;
 }
+bool hyperStrict(list<int> lst){
+  if(lst.empty())return true;
+  for(list<int>::iterator itr=lst.begin();next(itr)!=lst.end();itr++){
+    if((*itr)-*next(itr)<2)return false;
+  }
+  return true;
+}
+bool diffAtDist(list<int> lst,int dist,int diff){
+  if(lst.size()<dist)return true;
+  for(list<int>::iterator itr=lst.begin();next(itr,dist)!=lst.end();itr++){
+    if((*itr)-*next(itr,dist)<diff)return false;
+  }
+  return true;
+  
+}
+bool congruenceAtDist(list<int> lst,int A,int B,int C,int D){
+  // diff of lamda_j and lamda_{j+A} is less than or equal to B only if sum from lamda_j to lamda_{j+A} is congruence to C (mod D)
+  if(lst.size()<A)return true;
+  for(list<int>::iterator itr=lst.begin();next(itr,A)!=lst.end();itr++){
+    if((*itr)-*next(itr,A)<=B){
+      int sum=0;
+      for(list<int>::iterator jtr=itr;jtr!=next(itr,A+1);jtr++){
+        sum+=*(jtr);
+      }
+      if(sum%D!=C)return false;
+    }
+  }
+  return true;
+}
+bool smallestPart(list<int> lst,int n){
+  if(lst.empty())return true;
+  return *prev(lst.end())>=n;
+}
+
+bool I1(list<int> lst){
+  return diffAtDist(lst,2,3)&&congruenceAtDist(lst,1,1,0,3)&&smallestPart(lst,1);
+}
+bool I2(list<int> lst){
+  return diffAtDist(lst,2,3)&&congruenceAtDist(lst,1,1,0,3)&&smallestPart(lst,2);
+}
+bool I3(list<int> lst){
+  return diffAtDist(lst,2,3)&&congruenceAtDist(lst,1,1,0,3)&&smallestPart(lst,3);
+}
+bool I4(list<int> lst){
+  return diffAtDist(lst,2,3)&&congruenceAtDist(lst,1,1,2,3)&&smallestPart(lst,2);
+}
+bool I5(list<int> lst){
+  list<int> tmpLst(lst);
+  if(!tmpLst.empty())tmpLst.pop_back();
+  return diffAtDist(lst,3,3)&&congruenceAtDist(lst,2,1,1,3)&&smallestPart(tmpLst,2);
+}
+bool I6(list<int> lst){
+  list<int> tmpLst(lst);
+  if(!tmpLst.empty())tmpLst.pop_back();
+  return diffAtDist(lst,3,3)&&congruenceAtDist(lst,2,1,2,3)&&smallestPart(tmpLst,3)&&smallestPart(lst,2);
+}
+
+bool euler(list<int> lst){
+  return diffAtDist(lst,1,1);
+}
+bool RogersRamanujan(list<int> lst){
+  return diffAtDist(lst,1,2);
+}
 
 bool checkConditions(list<int> lst){
-  return strict(lst);
+  return diffAtDist(lst,1,2);
 }
 
 int generatePartition(int n,int tail,int sum,list<int> prev){
