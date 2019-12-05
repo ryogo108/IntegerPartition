eabf7b91f9f4ec2f6afd7cfcee4de79964c8f664
ryogo <ryogo.1008@gmail.com>
2019-12-05 18:46:40 +0900

    some update

--- IdentityFinder/IdentityFinder2.cpp
+++ IdentityFinder/IdentityFinder2.cpp
@@ -94,12 +94,21 @@ void SearchSp_Original7(int n){
   }
 }
 void SearchSp_GeneralSchur(int n){
-  rep(np,256)rep(af,2)rep(dist,4)rep(diff,11)rep(residue,1024){
-    bool f = (diff>=1 && dist>=1);
-		f=f&&(residue<=pow(2,diff));
+  rep(np,256)rep(D,9)rep(dist,4)rep(diff,9)rep(residue,256){
+    bool f = (diff>=1 && dist>=1 && D>=1);
+		f=f&&(residue<pow(2,diff)&&np<pow(2,D));
     if(!f)continue;
-    vector<int>params={np,af,dist,diff,residue};
+    vector<int>params={np,D,dist,diff,residue};
     vector<long long>count=countPartitions(n,partitions,generateConditionsGeneralSchur(params));
+    vector<long long> A=Factor(count);
+    printPeriodOfSeq(A); 
+  }
+}
+void SearchSp_GeneralSchur2(int n){
+  rep(k,n/4+1){
+		if(k==0)continue;
+    vector<int>params={k};
+    vector<long long>count=countPartitions(n,partitions,generateConditionsGeneralSchur2(params));
     vector<long long> A=Factor(count);
     printPeriodOfSeq(A); 
   }
@@ -116,5 +125,5 @@ void SearchSp_GeneralGollnitz(int n){
 int main(int argc,char *argv[]){
   int n=atoi(argv[1]);
   generatePartition(n,partitions);
-	SearchSp_GeneralSchur(n);
+	SearchSp_GeneralSchur2(n);
 }
