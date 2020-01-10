7302217f798990586df550c1646f21e9bf452a8c
RyogoTabuchi <ryogo.1008@gmail.com>
2020-01-10 15:44:10 +0900

    some update

--- IdentityFinder/IdentityChecker2.cpp
+++ IdentityFinder/IdentityChecker2.cpp
@@ -1,30 +1,40 @@
 #include"common.cpp"
 vector<part> partitions;
 int k=3;
+
+int gcd(int a,int b){
+  if(b==0)return a;
+  if(b>a)return gcd(b,a);
+  return gcd(b,a%b);
+}
+
 bool checkConditions(Par & p){	
-	const int D=2*k;
-	const int ap=(1<<1)|(1<<((1+k)%D))|(1<<((2+k)%D));
-	const int dist=1;
-	const int diff=D;
-	const int residue=(1<<1)|(1<<((1+k)%D));
-	if(D==0 || diff==0)return true;
-	bool f=true;
-	for(int i=0;i<p.size();i++){
-		if(p[i]==0)break;
-		else{
-			f=f&&((ap>>((p[i]%D))&1)==1);
-		}
-		if(p[i+dist]!=0){
-			f=f&&(p[i]-p[i+dist]>=diff);
-			if(p[i]-p[i+dist]==diff){
-				bool tmp=false;
-				for(int j=0;j<=diff;j++){
-					if((residue>>(j))&1)tmp=tmp||(p[i]%(diff)==j);
-				}
-				f=f&&tmp;
-			}
-		}
-	}
+  bool f=true;
+	return f;
+}
+bool checkConditions1(Par & v){	
+  bool f=true;
+  int cnt=0;
+  for(int i=0;i<v.size();i++){
+    if(v[i]==0)break;
+    if(i==0){
+      cnt++;
+      continue;
+    }
+    if(v[i]==v[i-1]){
+      cnt++;
+    }
+    else cnt=1;
+    if(cnt>=k)f=false;
+  }
+	return f;
+}
+bool checkConditions2(Par & v){	
+  bool f=true;
+  for(int i=0;i<v.size();i++){
+    if(v[i]==0)break;
+    f=f&&(gcd(v[i],k)==1);
+  }
 	return f;
 }
 
@@ -32,9 +42,11 @@ int main(int argc,char *argv[]){
   int n=atoi(argv[1]);
 	k=atoi(argv[2]);
   generatePartition(n,partitions);
-	vector<long long>v(countPartitions(n,partitions,checkConditions));
-	print_vector(v);
-	vector<long long>A(Factor(v));
+	vector<long long>v1(countPartitions(n,partitions,checkConditions1));
+	vector<long long>v2(countPartitions(n,partitions,checkConditions2));
+  print_vector(v1);
+  print_vector(v2);
+	/*vector<long long>A(Factor(v));
 	print_vector(A);
-	printPeriodOfSeq(A);
+	printPeriodOfSeq(A);*/
 }
