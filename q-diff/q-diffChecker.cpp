422417197402d683c2d672529dd7f4f2df1f1505
RyogoTabuchi <ryogo.1008@gmail.com>
2020-01-17 12:25:04 +0900

    Update q-diffChecker.cpp

--- q-diff/q-diffChecker.cpp
+++ q-diff/q-diffChecker.cpp
@@ -8,40 +8,25 @@ bool checkConditions(Par & p){
 	bool f=true;
 	for(int i=0;i<p.size();i++){
 		if(p[i]==0)break;
-    f=f&&(p[i]%6==1 || p[i]%6==4 || p[i]%6==5);
+    f=f&&(p[i]%4!=2);
 		if(p[i+1]!=0){
-      f=f&&(p[i]-p[i+1]>=6);
-      if(p[i]-p[i+1]==6)f=f&&(p[i]%3==1);
-    }
-	}
-	return f;
-}
-bool checkConditions2(Par & p){
-	bool f=true;
-	for(int i=0;i<p.size();i++){
-		if(p[i]==0)break;
-    f=f&&(p[i]%3==1);
-		if(p[i+1]!=0){
-      f=f&&(p[i]-p[i+1]>=1);
+      f=f&&(p[i]-p[i+1]>=4);
+      if(p[i]-p[i+1]==4)f=f&&(p[i]%2==1);
     }
 	}
 	return f;
 }
 
 void check_qdiff(const Polynomial & p){
-	const int order=4;
+	const int order=2;
 	vector<Polynomial>coefs(order+1);
 	vector<Polynomial>shifted(order+1);
-  coefs[0]=qShift(Polynomial({{0,1}}),6)-Polynomial({{1}});
-  coefs[1]=-coefs[0];
-  coefs[2]=qShift(Polynomial({{0,-1}}),1)*Polynomial({{-1},{0},{0},{0},{1}});
-  coefs[3]=qShift(Polynomial({{0,-1}}),5)*(qShift(Polynomial({{0,1}}),2)-Polynomial({{1}}));
-  coefs[4]=qShift(Polynomial({{0,-1}}),4)*(qShift(Polynomial({{0,1}}),2)-Polynomial({{1}}));
+  coefs[0]=Polynomial({{1}});
+  coefs[1]=-Polynomial({{1},{0,1},{0},{0,1}});
+  coefs[2]=-qShift(Polynomial({{0,1}}),4)*Polynomial({{1},{0},{0},{0},{0,-1}});
   shifted[0]=p;
-  shifted[1]=qShift(p,2);
-  shifted[2]=qShift(p,4);
-  shifted[3]=qShift(p,6);
-  shifted[4]=qShift(p,8);
+  shifted[1]=qShift(p,4);
+  shifted[2]=qShift(p,8);
   
   
 	Polynomial diff;
@@ -65,13 +50,7 @@ void test(Polynomial & p1,Polynomial & p2){
 int main(int argc,char *argv[]){
   int n=atoi(argv[1]);
   generatePartition(n,partitions);
-	Polynomial p=countFinePartitions2(n,partitions,checkConditions);
-  Polynomial q=countFinePartitions(n,partitions,checkConditions2);
-	//check_qdiff(p);
-  Polynomial r=p-q;
-  print_Polynomial(p);
-  print_Polynomial(q);
-  cout<<"p-q="<<endl;
-  print_Polynomial(r);
+  Polynomial p=countFinePartitions(n,partitions,checkConditions);
+	check_qdiff(p);
 }
 
