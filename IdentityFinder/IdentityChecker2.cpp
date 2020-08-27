252eda6d0de71ae63cef63e6d5f98822e2777df6
RyogoTabuchi <ryogo.1008@gmail.com>
2020-08-27 14:58:55 +0900

    Rename function and variable names

--- IdentityFinder/IdentityChecker2.cpp
+++ IdentityFinder/IdentityChecker2.cpp
@@ -1,35 +1,32 @@
 #include"common.cpp"
 #include<vector>
 vector<part> partitions;
-bool checkConditions(Par & p){
-  bool f=true;
-  bool tmp=false;
-  int pool;
-  int l=lengthOfPartition(p);
-  vector<int> sub;
+bool isSuitablePartition(Par & p){
+  bool isSuitable=true;
+  int length=lengthOfPartition(p);
   for(int i=0;i<p.size();i++){
     if(p[i]==0)break;
     if(p[i+1]!=0){
-      if(p[i]==p[i+1])f=f&&(p[i]%5==0);
-      if(p[i]%5==3)f=f&&(p[i+1]!=p[i]-1);
-      if(p[i]%5==1)f=f&&(p[i+1]!=p[i]-2);
+      if(p[i]==p[i+1])isSuitable &= (p[i]%5==0);
+      if(p[i]%5==3)isSuitable &= (p[i+1]!=p[i]-1);
+      if(p[i]%5==1)isSuitable &= (p[i+1]!=p[i]-2);
       if(p[i+2]!=0){
-        f=f&&(p[i]-p[i+2]>=5);
-        if(p[i]%5==0)f=f&&(p[i]-p[i+2]>5);
+        isSuitable &= (p[i]-p[i+2]>=5);
+        if(p[i]%5==0)isSuitable &= (p[i]-p[i+2]>5);
         if(p[i+3]!=0){
-          if(p[i]%5==1)f=f&&(!(p[i+3]==p[i]-7));
+          if(p[i]%5==1)isSuitable &= (!(p[i+3]==p[i]-7));
         }
       }
     }
   }
-  return f;
+  return isSuitable;
 }
 
 int main(int argc,char *argv[]){
   int n=atoi(argv[1]);
   generatePartition(n,partitions);
 	//vector<long long>v(countPartitions(n,partitions,checkConditions));
-	vector<long long>v(countPartitionsWithPirnt(n,partitions,checkConditions));
+	vector<long long>v(countPartitionsWithPirnt(n,partitions,isSuitablePartition));
 	printVector(v);
 	vector<long long>A(Factor(v));
 	printVector(A);
