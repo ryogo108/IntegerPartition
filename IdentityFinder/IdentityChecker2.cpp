a8bfe3728c3c4a099fd27f2df3930d2bc9797003
RyogoTabuchi <ryogo.1008@gmail.com>
2020-08-28 13:50:24 +0900

    Format codes

--- IdentityFinder/IdentityChecker2.cpp
+++ IdentityFinder/IdentityChecker2.cpp
@@ -12,12 +12,12 @@ const bool countWithPrint = false;
 const bool generateStrictPartitions = true;
 
 bool isSuitablePartition(Par & p){
-  bool isSuitable=true;
-  int length=lengthOfPartition(p);
-  for(int i=0;i<p.size();i++){
-    if(p[i]==0)break;
-    if(p[i+1]!=0){
-      isSuitable &= p[i]-p[i+1]>=2;
+  bool isSuitable = true;
+  int length = lengthOfPartition( p );
+  for(int i = 0; i < p.size(); i++){
+    if(p[ i ] == 0) break;
+    if(p[ i + 1 ] != 0){
+      isSuitable &= p[ i ] - p[ i+1 ] >= 2;
 //      if(p[i]==p[i+1])isSuitable &= (p[i]%5==0);
 //      if(p[i]%5==3)isSuitable &= (p[i+1]!=p[i]-1);
 //      if(p[i]%5==1)isSuitable &= (p[i+1]!=p[i]-2);
@@ -33,13 +33,13 @@ bool isSuitablePartition(Par & p){
   return isSuitable;
 }
 
-int main(int argc,char *argv[]){
-  int maxPartitionSize=atoi(argv[1]);
-  generatePartition(maxPartitionSize,partitions,generateStrictPartitions);
+int main(int argc, char * argv[]){
+  int maxPartitionSize = atoi( argv[1] );
+  generatePartition( maxPartitionSize, partitions, generateStrictPartitions );
   //printVector(partitions); // for debug
 	vector<long long> numOfPartitionsBySize( countSuitablePartitions( maxPartitionSize, partitions, isSuitablePartition, countWithPrint ) );
-	printVector(numOfPartitionsBySize);
-	vector<long long>exponentSeqOfFactoredGeneratingFunction(Factor(numOfPartitionsBySize));
-	printVector(exponentSeqOfFactoredGeneratingFunction);
-	printPeriodOfSeq(exponentSeqOfFactoredGeneratingFunction);
+	printVector( numOfPartitionsBySize );
+	vector<long long> exponentSeqOfFactoredGeneratingFunction( Factor( numOfPartitionsBySize ) );
+	printVector( exponentSeqOfFactoredGeneratingFunction );
+	printPeriodOfSeq( exponentSeqOfFactoredGeneratingFunction );
 }
