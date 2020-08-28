6f4443726ed1e9f2692d03ff5a78e5c7c6820886
RyogoTabuchi <ryogo.1008@gmail.com>
2020-08-28 13:03:24 +0900

    Use Rogers-Ramanujan condition

--- IdentityFinder/IdentityChecker2.cpp
+++ IdentityFinder/IdentityChecker2.cpp
@@ -7,16 +7,17 @@ bool isSuitablePartition(Par & p){
   for(int i=0;i<p.size();i++){
     if(p[i]==0)break;
     if(p[i+1]!=0){
-      if(p[i]==p[i+1])isSuitable &= (p[i]%5==0);
-      if(p[i]%5==3)isSuitable &= (p[i+1]!=p[i]-1);
-      if(p[i]%5==1)isSuitable &= (p[i+1]!=p[i]-2);
-      if(p[i+2]!=0){
-        isSuitable &= (p[i]-p[i+2]>=5);
-        if(p[i]%5==0)isSuitable &= (p[i]-p[i+2]>5);
-        if(p[i+3]!=0){
-          if(p[i]%5==1)isSuitable &= (!(p[i+3]==p[i]-7));
-        }
-      }
+      isSuitable &= p[i]-p[i+1]>=2;
+//      if(p[i]==p[i+1])isSuitable &= (p[i]%5==0);
+//      if(p[i]%5==3)isSuitable &= (p[i+1]!=p[i]-1);
+//      if(p[i]%5==1)isSuitable &= (p[i+1]!=p[i]-2);
+//      if(p[i+2]!=0){
+//        isSuitable &= (p[i]-p[i+2]>=5);
+//        if(p[i]%5==0)isSuitable &= (p[i]-p[i+2]>5);
+//        if(p[i+3]!=0){
+//          if(p[i]%5==1)isSuitable &= (!(p[i+3]==p[i]-7));
+//        }
+//      }
     }
   }
   return isSuitable;
