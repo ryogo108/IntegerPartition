3c19c15d1091971231510017436b561d19611137
RyogoTabuchi <ryogo.1008@gmail.com>
2020-08-30 16:47:46 +0900

    Add forbidden pattern memo about forbid 2_a1 and forbid 2_a2

--- IdentityFinder/countS5WeightedPartitions.cpp
+++ IdentityFinder/countS5WeightedPartitions.cpp
@@ -63,13 +63,14 @@ bool checkDiff6ColorCondition(part l, part r){
 // Done : Tsuchioka, Watanabe を参考に上の weighted における禁止パターンを追加したものを C_prototype として #( 6ColorStrict & C_prototype )( n ) >= #4ColorStrict( n ) であるかどうかを確かめた. これが成り立たなければおそらくこの weighted word は存在しない.
 // todo : 上の "おそらくこの weighted word は存在しない" を厳密に書き下す.
 // issue : 2_a2, 2_a1 がそれぞれ現れる. これらは S5 への変換で 2_a2 -> 5, 2_a1 -> 5 となり、#( 6ColorStrict & C )( 2 ) == #4ColorStrict( 2 ) + 1 である. 単純には 2_a2 もしくは 2_a1 を禁止列にすることが考えられる.
-// Forbidden pattern todo : forbid 2_a1 を追加して調べる.
-// Forbidden pattern todo : forbid 2_a2 を追加して調べる.
+//    Forbidden pattern memo : forbid 2_a1 を追加して It's OK under 15.
+//    Forbidden pattern memo : forbid 2_a2 を追加して It's OK under 15.
 bool isSuitablePartition(Par & p){
   bool isSuitable = true;
   for(int i = 0; i < p.size(); i++){
     if(p[ i ] == 0) break;
     isSuitable &= !(colorOf6ColorPart( p[ i ] ) == a1 && absOf6ColorPart( p[ i ] ) == 1)&&!(colorOf6ColorPart( p[ i ] ) == a2 && absOf6ColorPart( p[ i ] ) == 1);
+    isSuitable &= !(colorOf6ColorPart( p[ i ] ) == a1 && absOf6ColorPart( p[ i ] ) == 2);
     if(p[ i + 1 ] != 0){
       isSuitable &= p[ i ] - p[ i + 1 ] >= 1;
       isSuitable &= !(absOf6ColorPart( p[ i ] ) == absOf6ColorPart( p[ i + 1 ]) && colorOf6ColorPart( p[ i ]) == a5 && colorOf6ColorPart( p[ i + 1] ) == a4);
