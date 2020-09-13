2cc25bda9674a011f3c00367e3cf5e11de4903bd
RyogoTabuchi <ryogo.1008@gmail.com>
2020-09-13 09:26:23 +0900

    Remove dead code

--- IdentityFinder/countS5.cpp
+++ IdentityFinder/countS5.cpp
@@ -5,7 +5,7 @@ vector<part> partitions;
 
 // countSuitablePartitions で Suitable な分割を表示するかどうかのフラグ
 // For debug
-const bool countWithPrint = false;
+const bool countWithPrint = true;
 
 // Strict 分割だけを生成するかどうかフラグ.
 // もし現れる分割が全て Strict ならこれを true にすると高速化される.
@@ -35,54 +35,17 @@ bool isSuitablePartition(Par & p){
   return isSuitable;
 }
 
-// partition を Σceil( ( λ_i + 1 ) / 5 ) として計算する.
-part roughSumOfPartition(Par partition){
-  part sum = part(0);
-  for(int i = 0; i < partition.size(); i++){
-    if(partition[i] == 0)break;
-    sum += (partition[ i ] + 1 ) / 5 + ( ( ( partition[ i ] + 1 ) % 5 )  != 0 );
-  }
-  return sum;
-}
-
-// roughSumOfPartition を使って分割を数え上げる.
-vector<long long> countSuitablePartitionsByRoghWeight(int maxSizeOfPartition, vector<part> rawPartitions){
-  vector<long long> numOfSuitablePartitionsBySize( maxSizeOfPartition + 1 );
-  Par examinedPartition;
-  examinedPartition.reserve( maxSizeOfPartition + 1 );
-  for(auto itr = rawPartitions.begin(); itr != rawPartitions.end(); itr++){
-    if(*itr == part(0)){
-      examinedPartition.push_back(part(0)); //0は分割の終端を表す.
-      part sumOfExaminedPartition = roughSumOfPartition(examinedPartition);
-      if( isSuitablePartition( examinedPartition ) && sumOfExaminedPartition <= maxSizeOfPartition){
-        numOfSuitablePartitionsBySize[ sumOfExaminedPartition ]++;
-        //if( withPrint ) printPartition( examinedPartition );
-      }
-      examinedPartition.clear();
-    }
-    else {
-      examinedPartition.push_back( *itr );
-    }
-  }
-  return numOfSuitablePartitionsBySize;
-}
-
 int main(int argc, char * argv[]){
   // 実行時に生成する分割の大きさの上限をコマンドライン引数にとる.
   int maxPartitionSize = atoi( argv[1] );
 
   // 和が maxPartitionSize 以下の分割を生成して partitions に一列で保存する.
   // 長さの短い順に保存されていて各分割の終端は 0 で区切られる.
-  // Option : generateStrictPartitions = true にすると Strict な分割に限って生成する.
-  // roughSumOfPartition で分割の大きさが 1 / 5 になるので 5 倍の大きさで分割を生成する.
-  generatePartition( 5 * maxPartitionSize, partitions, generateStrictPartitions );
-
-  // printVector(partitions); // for debug
+  generatePartition( maxPartitionSize, partitions, generateStrictPartitions );
 
   // partitions のうちで Suitable な分割を大きさごとに数える
   // Option : countWithPrint = true として Suitable な分割を数え上げと同時に表示する.
-  vector<long long> numOfPartitionsBySize( countSuitablePartitionsByRoghWeight( maxPartitionSize, partitions) );
-  //vector<long long> numOfPartitionsBySize( countSuitablePartitions( maxPartitionSize, partitions, isSuitablePartition, countWithPrint) );
+  vector<long long> numOfPartitionsBySize( countSuitablePartitions( maxPartitionSize, partitions, isSuitablePartition, countWithPrint) );
 
   // numOfPartitionsBySize を表示する.
 	printVector( numOfPartitionsBySize );
