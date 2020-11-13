fda7020698f5630f7ec4988e2bc4f90b43505955
RyogoTabuchi <ryogo.1008@gmail.com>
2020-11-14 00:50:02 +0900

    Fix names of variables

--- IdentityFinder2/IdentityCheckerByDiffMatrix.cpp
+++ IdentityFinder2/IdentityCheckerByDiffMatrix.cpp
@@ -4,11 +4,11 @@
 
 using namespace std;
 
-const int MAX_N = 100;
+const int MAX_PARTITION_SIZE = 100;
+const int MAX_PART = 100;
 const int MAX_MOD = 20;
-long long dp[MAX_N + 5][MAX_N + 5][MAX_MOD + 1];
 
-int max(int a,int b){
+int max(int a, int b){
   return a > b ? a : b;
 }
 
@@ -88,12 +88,15 @@ bool checkSeq(vector<long long> & Seq){
 }
 
 vector<long long> countPartitionsByDiffMatrix(const Mat<int> & diffMatrix, const vector<int> & forbiddenParts){
-  vector<long long> numOfPartitions(MAX_N + 1, 0);
+  vector<long long> numOfPartitions(MAX_PARTITION_SIZE + 1, 0);
 
-  int mod = diffMatrix.size();
+  int M = diffMatrix.size();
 
-  for(int partitionSize = 0; partitionSize <= MAX_N; partitionSize++){
-    for(int maximalPart = 0; maximalPart <= MAX_N; maximalPart++){
+  // dp[n][N][r] := #{ |λ| = n,  λ_1 <= N, λ_1 ≡r (mod M)}
+  long long dp[MAX_PARTITION_SIZE + 1][MAX_PART + 1][MAX_MOD + 1];
+
+  for(int partitionSize = 0; partitionSize <= MAX_PARTITION_SIZE; partitionSize++){
+    for(int maximalPart = 0; maximalPart <= MAX_PART; maximalPart++){
       for(int residue = 0; residue <= MAX_MOD; residue++){
         dp[partitionSize][maximalPart][residue] = 0;
       }
@@ -101,9 +104,9 @@ vector<long long> countPartitionsByDiffMatrix(const Mat<int> & diffMatrix, const
   }
   dp[0][0][0] = 1;
 
-  for(int partitionSize = 0; partitionSize <= MAX_N; partitionSize++){
-    for(int maximalPart = 1; maximalPart <= MAX_N; maximalPart++){
-      for(int residue = 0; residue < mod; residue++){
+  for(int partitionSize = 0; partitionSize <= MAX_PARTITION_SIZE; partitionSize++){
+    for(int maximalPart = 1; maximalPart <= MAX_PART; maximalPart++){
+      for(int residue = 0; residue < M; residue++){
         dp[partitionSize][maximalPart][residue] = dp[partitionSize][maximalPart - 1][residue];
       }
 
@@ -113,14 +116,14 @@ vector<long long> countPartitionsByDiffMatrix(const Mat<int> & diffMatrix, const
         continue;
       }
 
-      for(int residue = 0; residue < mod; residue++){
-        int diff = diffMatrix[maximalPart % mod][residue];
-        dp[partitionSize][maximalPart][maximalPart % mod] +=
+      for(int residue = 0; residue < M; residue++){
+        int diff = diffMatrix[maximalPart % M][residue];
+        dp[partitionSize][maximalPart][maximalPart % M] +=
           dp[partitionSize - maximalPart][max(maximalPart - diff, 0)][residue];
       }
     }
 
-    for(int residue = 0; residue < mod; residue++){
+    for(int residue = 0; residue < M; residue++){
       numOfPartitions[partitionSize] += dp[partitionSize][partitionSize][residue];
     }
   }
@@ -129,9 +132,7 @@ vector<long long> countPartitionsByDiffMatrix(const Mat<int> & diffMatrix, const
 }
 
 int main(){
-  Mat<int> ker, shift;
-
-  ker={
+  Mat<int> seed={
     {1, 1, 2, 2, 2, 2, 2, 2},
     {1, 1, 1, 1, 1, 2, 2, 2},
     {1, 1, 2, 2, 2, 2, 2, 2},
@@ -142,7 +143,7 @@ int main(){
     {0, 0, 1, 1, 1, 1, 1, 1},
   };
 
-  shift={
+  Mat<int> shift={
     {0, -1, -2, -3, -4, -5, -6, -7},
     {1, 0, -1, -2, -3, -4, -5, -6},
     {2, 1, 0, -1, -2, -3, -4, -5},
@@ -155,7 +156,7 @@ int main(){
 
   vector<int> forbiddenParts = {2};
 
-  Mat<int> diffMatrix = int(ker.size()) * ker + shift;
+  Mat<int> diffMatrix = int(seed.size()) * seed + shift;
   printMatrix(diffMatrix);
 
   vector<long long> numOfPartitions = countPartitionsByDiffMatrix(diffMatrix, forbiddenParts);
