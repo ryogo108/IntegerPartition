1bf7888ef44708639b6f55aa55abffc64dfd431e
RyogoTabuchi <ryogo.1008@gmail.com>
2020-11-13 21:02:23 +0900

    Refactor countPartitionsByDiffMatrix

--- IdentityFinder2/IdentityCheckerByDiffMatrix.cpp
+++ IdentityFinder2/IdentityCheckerByDiffMatrix.cpp
@@ -7,7 +7,6 @@ using namespace std;
 const int MAX_N=100;
 const int MAX_MOD=20;
 long long dp[MAX_N+5][MAX_N+5][MAX_MOD+1];
-vector<long long>A(MAX_N+1);
 
 int max(int a,int b){
   return a>b?a:b;
@@ -81,45 +80,45 @@ bool checkSeq(vector<long long> & Seq){
   return false;
 }
 
-void countPartition(Mat<int> ker, Mat<int> shift){
-  int mod = ker.size();
-  Mat<int> data = mod * ker + shift;
+vector<long long> countPartitionsByDiffMatrix(const Mat<int> & diffMatrix){
+  vector<long long> numOfPartitions(MAX_N + 1, 0);
 
-  fill(A.begin(), A.end(), 0);
+  int mod = diffMatrix.size();
 
-  for(int i = 0; i < MAX_N + 5; i++){
-    for(int j = 0; j < MAX_N + 5; j++){
-      for(int k = 0; k < MAX_MOD + 1; k++){
-        dp[i][j][k] = 0;
+  for(int partitionSize = 0; partitionSize < MAX_N + 5; partitionSize++){
+    for(int maximalPart = 0; maximalPart < MAX_N + 5; maximalPart++){
+      for(int residue = 0; residue < MAX_MOD + 1; residue++){
+        dp[partitionSize][maximalPart][residue] = 0;
       }
     }
   }
-  dp[0][0][0]=1;
+  dp[0][0][0] = 1;
 
-  for(int i = 0; i < MAX_N + 1; i++){
-    for(int j = 1; j < MAX_N + 1; j++){
-      for(int k = 0; k < mod; k++){
-        dp[i][j][k] = dp[i][j - 1][k];
-        if(i >= j && j % mod == k && j != 2){
-          for(int r = 0; r < mod; r++){
-              dp[i][j][k] += dp[i - j][max(j - data[k][r], 0)][r];
-          }
-        }
+  for(int partitionSize = 0; partitionSize <= MAX_N; partitionSize++){
+    for(int maximalPart = 1; maximalPart <= MAX_N; maximalPart++){
+      for(int residue = 0; residue < mod; residue++){
+        dp[partitionSize][maximalPart][residue] = dp[partitionSize][maximalPart - 1][residue];
+      }
+
+      if(maximalPart > partitionSize) continue;
+
+      if(maximalPart == 2){
+        continue;
+      }
+
+      for(int residue = 0; residue < mod; residue++){
+        int diff = diffMatrix[maximalPart % mod][residue];
+        dp[partitionSize][maximalPart][maximalPart % mod] +=
+          dp[partitionSize - maximalPart][max(maximalPart - diff, 0)][residue];
       }
     }
 
-    for(int r = 0; r < mod; r++){
-      A[i] += dp[i][i][r];
+    for(int residue = 0; residue < mod; residue++){
+      numOfPartitions[partitionSize] += dp[partitionSize][partitionSize][residue];
     }
   }
 
-  if(checkSeq(A)){
-    cout << "dil : " << mod << endl;
-    cout << "ker : " << endl;
-    printMatrix(ker);
-    cout << "shift : " << endl;
-    printMatrix(shift);
-  }
+  return numOfPartitions;
 }
 
 int main(){
@@ -147,5 +146,9 @@ int main(){
     {7, 6, 5, 4, 3, 2, 1, 0},
   };
 
-  countPartition(ker,shift);
+  Mat<int> diffMatrix = int(ker.size()) * ker + shift;
+  printMatrix(diffMatrix);
+
+  vector<long long> numOfPartitions = countPartitionsByDiffMatrix(diffMatrix);
+  checkSeq(numOfPartitions);
 }
